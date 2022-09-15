6e6b6c9bb8634b013d1de5bafac6fdb4299d71ab
Michael Yiu <maplepie32@gmail.com>
2022-09-15 12:53:03 -0700

    created array for user ingredient

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ submitButton = $('#submit-button');
 
 
 function getRecipes(){
-    console.log(userIngredients.val());
-
+    
     var checkedOptions = [];
 
     for(i=0; i<dietFilter.length; i++){
@@ -16,14 +15,38 @@ function getRecipes(){
             checkedOptions.push(filterItem);
         }
     }
-    console.log(checkedOptions);
+
+    if(checkedOptions.length >= 1){
+        var filterUrlText = 'Health'
+    }
+
+
+    var ingredients = userIngredients.val();
+    var ingredients = ingredients.replaceAll(',', '');
+    var ingredientsURL = ingredients.replaceAll(' ', '%20');
+    console.log(ingredients);
+
 }
+
+var n=2;
+
+//var edamamURL = 'https://api.edamam.com/api/nutrition-data?app_id=9640b10a&app_key=91c330f1156c5b44712e4a0dc56558f3&nutrition-type=cooking&ingr=1%20pound%20salmon';
+var edamamURL = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken%2C%20tomato%2C%20pasta&app_id=b9fe928c&app_key=db52e0a131b936a4951946eca553aa08&ingr=5&diet=balanced&health=fish-free&mealType=Dinner&calories=100-2000&time=60';
+
+if(n===5){
+    fetch(edamamURL)
+    .then( function(response) {
+        console.log(response.status);
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+};
 
 
 //url that includes the ingredients the user wants to eat
 var url = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=7eec931589c14d89b714a88f037239c1&ingredients=apples,+flour,+sugar&number=1'
-
-var n=6;
 
 if(n===3){
     fetch(url)
@@ -36,18 +59,6 @@ if(n===3){
     })
 }
 
-var edamamURL = 'https://api.edamam.com/api/nutrition-data?app_id=9640b10a&app_key=91c330f1156c5b44712e4a0dc56558f3&nutrition-type=cooking&ingr=1%20pound%20salmon';
-
-if(n===5){
-    fetch(edamamURL)
-    .then( function(response) {
-        console.log(response.status);
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data);
-    })
-};
 
 //Event listeners
 submitButton.on('click', getRecipes);
